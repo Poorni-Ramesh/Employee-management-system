7a8d80ec74978bec606f153742587667ce3eac17
Poorni-Ramesh <poornibuvana29@gmail.com>
2026-04-06 11:34:51 +0530

    Fix duplicate compilation error

--- ems-backend/ems-backend/src/main/java/com/ems/controller/AdminController.java
+++ ems-backend/ems-backend/src/main/java/com/ems/controller/AdminController.java
@@ -17,7 +17,6 @@ import com.ems.entity.Employee;
 import com.ems.service.EmployeeService;
 
 import jakarta.servlet.http.HttpServletRequest;
-@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/admin")
 @CrossOrigin(origins = "*")
